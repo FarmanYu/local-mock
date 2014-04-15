33f1d08e6557846df09b088d4b7aa682c73ac146
xiangwenwen <xiangwenwe@foxmail.com>
2014-04-15 13:43:49 +0800

    压缩了代码量

--- listen.js
+++ listen.js
@@ -1,66 +1,44 @@
-var CONFFILE = "./config.js";
 var http = require("http"),
     fs = require("fs"),
-    conf = require(CONFFILE),
-    urlParse = require("url").parse,
-    path = require("path"),
-    Mock = require("mockjs");
-
+    urlParse = require("url"),
+    path = require("path");
+try{
+  var conf = require('./config'),Mock = require("mockjs"),self;
+}catch(e){
+  console.log('not find module config or mock.js');
+  return false;
+}
 var Main = {
+  server:null,
   start: function() {
-    this.bindEvent();
-    this.update();
     console.log("Listener was start, at " + conf.PORT);
-  },
-  update: function() {
-    var self = this;
-    this.server = null;
-    this.server = http.createServer(function(request, res) {
-      self.bindBlankForRes(res);
-      var pathname = urlParse(request.url).pathname;
-      if (pathname.indexOf("favicon.ico") != -1) return;
-      console.log((new Date().toString().match(/\d{1,2}:\d{1,2}:\d{1,2}/)[0])+" : " + pathname);
-      var result = self.match(pathname);
-      if (result){
-        result = result.replace(/\//g, '\\');
-        var getewd = process.cwd()
-        var filePath = getewd + path.sep + 'data' + path.sep + result;
-        //support ajax Cross domain
-        res.setHeader("Access-Control-Allow-Headers","Content-Type, Accept");
-        res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE");
-        res.setHeader("Access-Control-Allow-Origin","*");
-        res.setHeader("Access-Control-Max-Age",30 * 24 * 3600);
-        try {
-          var stats = fs.statSync(filePath);
-        } catch (e) {
-          res.sendBlank();
-        }
-        if (stats && stats.isFile()) {
-          fs.readFile(filePath, "utf-8", function(err, template) {
-            if (err) {
-              res.sendBlank();
-            }
-            var json = {};
-            try{
-              json = JSON.parse(template);
-              json = Mock.mock(json);
-            } catch(e){
-              res.sendBlank();
-            }
-            res.end(JSON.stringify(json, null, 4));
-          });
-        } else {
-          res.sendBlank();
-        }
-      } else {
-        res.sendBlank();
+    self = this;
+    self.server = null;
+    self.server = http.createServer(function(request,res){
+      var pathname = urlParse.parse(request.url).pathname;
+      if(pathname !== '/favicon.ico'){
+          console.log((new Date().toString().match(/\d{1,2}:\d{1,2}:\d{1,2}/)[0])+" : " + pathname);
+          var result = self.match(pathname);
+          if(!result)self.errorHandler(res);
+          result = result.replace(/\//g, '\\');
+          var getewd = process.cwd()
+          var filePath = getewd + path.sep + result;
+          try{
+              var stats = fs.statSync(filePath);
+              if(!(stats && stats.isFile())){
+                  self.errorHandler(res);
+              }
+          }catch (e) {
+              self.errorHandler(res);
+          }
+          self.staticFileService(filePath,res);
       }
-    }).listen(conf.PORT);
+    });
+    return self.server;
   },
-  bindBlankForRes: function(res) {
-    res.sendBlank = function(){
-      this.end("{}");
-    }
+  errorHandler: function(res) {
+      res.writeHead('404',{'Content-Type': 'text/html'});
+      res.end("<h1>hi all this request is 404 or handler is bad</h1>");
   },
   match: function(pathname) {
     var paths = conf.paths;
@@ -75,12 +53,26 @@ var Main = {
     }
     return data;
   },
-  bindEvent: function() {
-    var self = this;
-    fs.watch(CONFFILE, function() {
-      console.log("config is update, please restart!");
-    })
+  staticFileService:function(filePath,res){
+    fs.readFile(filePath, "utf-8", function(err, template) {
+        if (err) {
+          self.errorHandler(res);
+        }
+        var json = {};
+        try{
+          json = JSON.parse(template);
+          json = Mock.mock(json);
+        } catch(e){
+          self.errorHandler(res);
+        }
+        res.writeHead('200',{
+          'Access-Control-Allow-Headers':'Content-Type, Accept',
+          'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE',
+          'Access-Control-Allow-Origin':'*',
+          'Access-Control-Max-Age':30 * 24 * 3600
+        });
+        res.end(JSON.stringify(json, null, 4));
+    });
   }
-
 };
-Main.start();
+Main.start().listen(conf.PORT);
